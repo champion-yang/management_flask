baba28772a5a47b732557c3f90f482d3c73a134b
yangjunlin <529352969@qq.com>
2020-09-03 14:57:26 +0800

    指定日志输出

--- logger.py
+++ logger.py
@@ -9,16 +9,32 @@ log_level = config_obj.get('logging', 'level') if config_obj.get('logging', 'lev
 
 
 def init_logger(logger_name):
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
     logger = logging.getLogger(logger_name)
     logger.setLevel(logging.DEBUG)
 
+    # handler all
     screen_handler = StreamHandler()
+    handler = TimedRotatingFileHandler('./logs/{}_all.logs'.format(logger_name), when='midnight', backupCount=7)
     datefmt = "%Y-%m-%d %H:%M:%S"
     format_str = "[%(asctime)s]: %(name)s %(levelname)s %(lineno)s %(message)s"
     formatter = logging.Formatter(format_str, datefmt)
     screen_handler.setFormatter(formatter)
     screen_handler.setLevel(logging.DEBUG)
+    handler.setFormatter(formatter)
+    handler.setLevel(eval("logging.{}".format(log_level.upper())))
     logger.addHandler(screen_handler)
+    logger.addHandler(handler)
+
+    # handler error
+    handler = TimedRotatingFileHandler('./logs/{}_error.logs'.format(logger_name), when='midnight', backupCount=7)
+    datefmt = "%Y-%m-%d %H:%M:%S"
+    format_str = "[%(asctime)s]: %(name)s %(levelname)s %(lineno)s %(message)s"
+    formatter = logging.Formatter(format_str, datefmt)
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.ERROR)
+    logger.addHandler(handler)
 
     log = logging.getLogger(logger_name)
 
