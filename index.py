a1be7fb0fa0bb521326a385047aab4e1e3faa808
yangjunlin <529352969@qq.com>
2020-09-04 12:38:07 +0800

    配置docker

--- index.py
+++ index.py
@@ -1,5 +1,17 @@
 from apps import app
+import os
+import socket
+
+
+@app.route("/")
+def hello():
+    visits = "<i>cannot connect to Redis, counter disabled</i>"
+
+    html = "<h3>Hello {name}!</h3>" \
+           "<b>Hostname:</b> {hostname}<br/>" \
+           "<b>Visits:</b> {visits}"
+    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=9527)
+    app.run(host="0.0.0.0", port=8000)
